359cfb0fb5c402b17bc5f39a5d3ce5c50eca1276
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-19 13:51:45 -0400

    fix

--- public/app.js
+++ public/app.js
@@ -1,9 +1,24 @@
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
-    // For each one
-    $("#articles").empty();
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#articles").append("<p data-id='" +  data[i]._id + "'>" + "<img src=" + data[i].image + "><br>" + "<a href='https://gamespot.com" + data[i].link + "'</a>" + data[i].title + "</p>");
-    }
-  });
+  $("#articles").empty();
+
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles").append(
+      "<div class='col l4 articleInfo'<p data-id='" +
+        data[i]._id +
+        "'>" +
+        "<img src=" +
+        data[i].image +
+        ">" +
+        "<a href='" +
+        data[i].link +
+        "'</a>" +
+        data[i].title +
+        "</p></div>"
+    );
+  }
+}); 
+
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
@@ -18,70 +19,74 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+//Connecting to the MongoDB with either the heroku add-on, or the localhosts' DB
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-console.log(MONGODB_URI)
 // Routes
 
+//Starting the scrape..
 app.get("/scrape", function(req, res) {
+  //Using axios to get the requested site
+  axios
+    .get("https://liveforlivemusic.com/news")
+    .then(function(response) {
+      //Using cheerio to load all of the data from the site
+      var $ = cheerio.load(response.data);
 
-  axios.get("https://liveforlivemusic.com/news").then(function(response) {
-
-  var $ = cheerio.load(response.data);
-
-    $("li").each(function(i, element) {
-
+      //Grabbing each element on the site by it's 'li' tag
+      $("li").each(function(i, element) {
         var result = {};
-
+        //Grabbing the link...have some children!
         result.link = $(this)
-        .children("div")
-        .children("div")
-        .children("a")
-        .attr("href");
-        
+          .children("div")
+          .children("div")
+          .children("a")
+          .attr("href");
+
+        //Grabbing the title...have some more children!
         result.title = $(this)
-        .children("div")
-        .children("div")
-        .children("h3")
-        .children("a")
-        .text();
+          .children("div")
+          .children("div")
+          .children("h3")
+          .children("a")
+          .text();
 
+        //Grabbing the image URL...children?
         result.image = $(this)
-        .children("div")
-        .children("div")
-        .children("a")
-        .children("img")
-        .attr("src");
-       
+          .children("div")
+          .children("div")
+          .children("a")
+          .children("img")
+          .attr("src");
+
+        //Create the article and store it into the MongoDB
         db.Article.create(result)
-        .then(function(dbArticle) {
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            return res.json(err);
+          });
+      });
 
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-
-          return res.json(err);
-        });
+      //Once succesfully scraped, redirect the user to the home page
+      res.redirect("/");
+    })
+    .catch(function(err) {
+      console.log(err);
     });
-
-
-    res.redirect("/");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
 });
 
-app.get("/", function (req, res) {
-    res.send(index.html);
+//Setting the default page to the index.html file
+app.get("/", function(req, res) {
+  res.send(index.html);
 });
 
-
+//Getting all of the articles scraped from the database and displaying it as JSON
 app.get("/articles", function(req, res) {
-
   db.Article.find({})
     .then(function(dbArticle) {
       res.json(dbArticle);
@@ -93,19 +98,18 @@ app.get("/articles", function(req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
- 
   db.Article.findOne({ _id: req.params.id })
-  .populate("note")
-  .then(function(dbArticle) {
+    .populate("note")
+    .then(function(dbArticle) {
       res.json(dbArticle);
     })
-  .catch(function(err) {
-    res.json(err);
-  });
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
+//Updating an articles' note based on it's ID
 app.post("/articles/:id", function(req, res) {
-
   db.Note.create(req.body)
     .then(function(dbNote) {
       return db.Article.findOneAndUpdate(
@@ -122,6 +126,7 @@ app.post("/articles/:id", function(req, res) {
     });
 });
 
+//Listening on Port...
 app.listen(PORT, function() {
   console.log("App running on port " + PORT);
 });
